e3ddfdec9f517251136bb630348e351266679f2a
serge <serge@localhost>
2021-04-04 15:33:18 +0200

    pb avec git précédent sur dosier blend

---
+++ blender_osc/scripts/once.py
@@ -0,0 +1,97 @@
+
+from time import time
+import json
+
+from bge import logic as gl
+from scripts.utils import get_all_objects, add_object, read_json
+from scripts.rs_utils import Filtre, get_points
+
+from oscpy.server import OSCThreadServer
+
+
+def default_handler(*args):
+    print("default_handler", args)
+
+
+def on_points(*args):
+    body = args[-1]
+    args = args[:-1]
+
+    gl.points = get_points(args)
+    gl.new = 1
+    # Durée en frame depuis la dernière réception
+    gl.tempo = gl.frame_number - gl.receive_at
+    gl.receive_at = gl.frame_number
+
+
+def parts(args, n):
+    points = []
+    nb = 18
+    # #print(args)
+    for i in range(nb):
+        points.append([args[n*i], args[n*i+1]])
+    # #print(points)
+    return points
+
+
+def osc_server_init():
+    gl.server = OSCThreadServer()
+    gl.server.listen('0.0.0.0', port=8003, default=True)
+    # Les callbacks du serveur
+    gl.server.default_handler = default_handler
+    gl.server.bind(b'/points', on_points)
+
+
+def main():
+    print("Lancement de once.py ...")
+
+    gl.all_obj = get_all_objects()
+    gl.cube = gl.all_obj["Cube"]
+    gl.metarig = gl.all_obj["metarig"]
+    gl.person = gl.all_obj["person"]
+
+    gl.spheres = []
+    # 18 est le  body au centre de 11 et 12
+    for i in ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+                "10", "11", "12", "13", "14", "15", "16", "17", "18"]:
+        gl.spheres.append(gl.all_obj[i])
+
+    # AA sur Text
+    for obj_name, obj in gl.all_obj.items():
+        if "Text" in obj_name:
+            obj.resolution = 64
+
+    gl.t = time()
+    gl.fps = 0
+    gl.server = None
+    gl.points = None
+    gl.frame_number = 0
+    gl.nums = 20
+    gl.new = 0
+    gl.receive_at = 0
+    gl.tempo = 0
+    gl.body_visible = 1
+    gl.person.visible = 0
+
+    gl.debug = 1  # 1=avec fichier enregistré
+    if gl.debug:
+        gl.data_4 = read_json("./scripts/4.json")
+        print("Nombre de frame 4 =", len(gl.data_4))
+        gl.data_7 = read_json("./scripts/7.json")
+        print("Nombre de frame 7 =", len(gl.data_7))
+        gl.data_14 = read_json("./scripts/14.json")
+        print("Nombre de frame 14 =", len(gl.data_14))
+        gl.data_big = read_json("./scripts/cap_2021_04_03_14_44_fps_14.json")
+        print("Nombre de frame big =", len(gl.data_big))
+        gl.data = gl.data_4
+    else:
+        osc_server_init()
+
+    # Le filtre Savonarol Brigowski de scipy
+    gl.filtre = Filtre(18, 15)
+
+    # Placement et échelle dans la scène
+    gl.scale = 1
+    gl.up_down = 1.5
+    gl.left_right = 0.2
+    gl.av_ar = -2.5
---
+++ blender_osc/scripts/rs_utils.py
@@ -0,0 +1,135 @@
+from time import time, sleep
+import json
+from collections import deque
+
+import numpy as np
+try:
+    from scipy.signal import savgol_filter
+    SCIPY = True
+except:
+    print("Vous devez installer scipy")
+    SCIPY = False
+
+class Filtre:
+    """Filtre les points reçus du RealSense
+    piles[17][2] = deque([1.937, -0.495, 0.144, 3.24], maxlen=4)
+    """
+
+    def __init__(self, nb_points=18, pile_size=20):
+        self.pile_size = pile_size
+        self.nb_points = nb_points
+        self.points = None
+
+        self.piles = [0]*nb_points
+        # Pour créer les queues: si créées en les définissant à la place du 0,
+        # ça crée des queues qui pointent toutes vers la même queue
+        for i in range(self.nb_points):  # 18
+            self.piles[i] = []
+            for j in range(3):
+                self.piles[i].append(deque(maxlen=pile_size))
+
+        # Filtre
+        self.window_length = self.get_window_length()
+        self.order = 4
+
+    def add(self, points):
+        """points = liste de 18 items, soit [1,2,3] soit None"""
+        # Si pas de points, on passe
+        self.points = points
+        if points:
+            for i in range(self.nb_points):  # 18
+                if points[i]:
+                    for j in range(3):  # 3
+                        self.piles[i][j].append(points[i][j])
+
+    def get_smooth_points(self):
+
+        new_points = [0]*18
+        for i in range(self.nb_points):  # 18
+            if self.points:
+                if self.points[i]:
+                    new_points[i] = []
+                    valid = True
+                    for j in range(3):
+                        lst = []
+                        for item in self.piles[i][j]:
+                            lst.append(item)
+                        if len(lst) < self.window_length:
+                            valid = False
+                        else:
+                            if SCIPY:
+                                sav = savgol_filter(lst, self.window_length, self.order)
+                                new_points[i].append(round(sav[-1], 3))
+                            else:
+                                new_points[i].append(round(self.piles[i][j][-1], 3))
+                    if not valid:
+                        new_points[i] = None
+                else:
+                    new_points[i] = None
+            else:
+                new_points = None
+
+        return new_points
+
+    def get_window_length(self):
+        """window_length=impair le plus grand dans la pile"""
+
+        if self.pile_size % 2 == 0:
+            window_length = self.pile_size - 1
+        else:
+            window_length = self.pile_size
+        return window_length
+
+
+def read_json(fichier):
+    try:
+        with open(fichier) as f:
+            data = json.load(f)
+        f.close()
+    except:
+        data = Nonefiltre
+        print("Fichier inexistant ou impossible à lire:")
+    return data
+
+
+def get_points(data):
+    """frame_data = list(coordonnées des points empilés d'une frame
+        soit 3*18 items avec:
+            mutipliées par 1000
+            les None sont remplacés par (-1000000, -1000000, -1000000)
+            le numéro du body doit être enlevé avant
+    """
+    nb = 18
+    if len(data) % 3 == 0 and len(data)/3 == nb:
+        points = []
+        for i in range(nb):
+            # data[de 0 à 54] n'est jamais None car vient de l'OSC
+            val = [ data[(3*i)],
+                    data[(3*i)+1],
+                    data[(3*i)+2]]
+            if val == [-1000000, -1000000, -1000000]:
+                points.append(None)
+            else:
+                # Less coords sont multipliées par 1000 avant envoi en OSC
+                # Permutation de y et z, z est la profondeur pour RS et OpenCV
+                # et inversion de l'axe des y en z
+                points.append([val[0]/1000, val[2]/1000, -val[1]/1000])
+    else:
+        points = None
+    return points
+
+
+if __name__ == '__main__':
+
+    filtre = Filtre(18, 20)
+    fichier = "7.json"
+    data = read_json(fichier)
+
+    for i in range(len(data)):
+        # [:-1] pour oter le numéro du body en fin de liste
+        points = get_points(data[i][:-1])
+        # #print("\npoints:", points)
+        filtre.add(points)
+        # #print("piles:", filtre.piles)
+        last_points = filtre.get_smooth_points()  # impair
+        print("last_points:", last_points)
